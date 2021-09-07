81bfa90d6ae5f09c82556d2fd864b1ccdf16aed0
Steve Liddle <liddle@byu.edu>
2021-09-07 10:11:55 -0600

    Initial Commit

---
+++ ConcentrationHW3/ConcentrationHW3App.swift
@@ -0,0 +1,17 @@
+//
+//  ConcentrationHW3App.swift
+//  ConcentrationHW3
+//
+//  Created by Steve Liddle on 9/7/21.
+//
+
+import SwiftUI
+
+@main
+struct ConcentrationHW3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
